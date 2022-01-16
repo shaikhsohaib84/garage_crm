7c11077247f5bdfb5b48957b5fda18514bf07244
sohaib shaikh <sohaib.shaikh@livehealth.in>
2022-01-16 16:29:13 +0530

    setup for react done

--- crm/crm/settings.py
+++ crm/crm/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'corsheaders',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -125,3 +128,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000"
+]
